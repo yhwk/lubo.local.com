99589013f076e051d4374775aa74a30713b00562
TbhT <hustapple132@gmail.com>
2016-07-09 15:09:51 +0800

    add sqlite3

--- back_end/login.php
+++ back_end/login.php
@@ -1,6 +1,18 @@
 <?php
-echo $_POST;
-echo "<h1>hello world</h1>";
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-?>
+    $type = $_POST['optionsRadios'];
+    $password = htmlspecialchars($_POST['password']);
+
+//    $db = new MySqlite();
+//    $query = $db->query("SELECT * FROM user");
+//    if (!$db) {
+//        echo $db->lastErrorMsg();
+//    } else {
+//        echo "Opened database successfully";
+//    }
+
+    header('Content-type: application/json');
+    print json_encode(array('type' => 'login'));
+};
